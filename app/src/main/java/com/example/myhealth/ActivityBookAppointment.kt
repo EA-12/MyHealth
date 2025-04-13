dc2f3aac732a7ef1d0428e3043048d5a25d28374
E-12 <evalcaide@gmail.com>
2025-04-13 13:40:28 +0200

    style(BookAppointment): mejora estilos de pantalla de petición de cita

--- app/src/main/java/com/example/myhealth/ActivityBookAppointment.kt
+++ app/src/main/java/com/example/myhealth/ActivityBookAppointment.kt
@@ -1,15 +1,26 @@
 package com.example.myhealth
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class ActivityBookAppointment : AppCompatActivity() {
+
+    private lateinit var editTextDate: EditText
+    private val calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,9 +30,47 @@ class ActivityBookAppointment : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        editTextDate = findViewById(R.id.editTextDate)
+
+        calendar.set(Calendar.YEAR, 2025)
+        calendar.set(Calendar.MONTH, Calendar.APRIL)
+        calendar.set(Calendar.DAY_OF_MONTH, 13)
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, day)
+            updateLabel()
+        }
+
+        editTextDate.setOnClickListener {
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+        updateLabel()
+
+        val timePicker = findViewById<TimePicker>(R.id.appointmentTimePicker)
+        timePicker.setIs24HourView(true)
+        timePicker.hour = 12
+        timePicker.minute = 0
     }
+
     fun goToMainActivity(view: View) {
+        Toast.makeText(this, "Cita guardada correctamente", Toast.LENGTH_SHORT).show()
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun updateLabel() {
+        val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        editTextDate.setText(format.format(calendar.time))
     }
 }
